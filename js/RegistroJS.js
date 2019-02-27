e3dd8746d7108da8381dabdfa3f8c39b8e2922b8
armandoalamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-02-26 22:34:13 -0600

    implementacion del letrero
    se valida en nombre y apellido

--- js/RegistroJS.js
+++ js/RegistroJS.js
@@ -1,6 +1,41 @@
+
+
 //boton de registro 
 document.getElementById("btnRegistro").onclick = function () {
-        location.href = "index.html";
 
-    };
+	
+    var $nombre = $("#first_name");
+    var $apellido = $("#last_name");
+
+
+    //validar si el field de nombre esta lleno
+	if($nombre.val()=='')
+	{
+		$("#btnRegistro").attr('data-target',"modal_nombre");
+		 
+
+		 $(document).ready(function(){
+    	$('.modal').modal();
+ 		});
+	}
+	else if($apellido.val()=='')
+	{
+		$("#btnRegistro").attr('data-target',"modal_apellido");
+		 
+
+		 $(document).ready(function(){
+    	$('.modal').modal();
+ 		});
+	}
+
+	
+
+
+
+};
+        
+  
+ 
+    
+
 
