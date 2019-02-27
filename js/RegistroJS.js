f59ed082d088007865e16af6150bfe333fb1bcd0
armandoalamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-02-26 23:27:20 -0600

    update Registro.html y RegistroJS.js
    casi se termina con la forma de registro...
    nada mas falta lo del logo

--- js/RegistroJS.js
+++ js/RegistroJS.js
@@ -6,6 +6,9 @@ document.getElementById("btnRegistro").onclick = function () {
 	
     var $nombre = $("#first_name");
     var $apellido = $("#last_name");
+    var $contrasena = $("#password");
+    var $contrasenaConfirma =$("#confirmPassword");
+    var $email = $("#email");
 
 
     //validar si el field de nombre esta lleno
@@ -17,7 +20,7 @@ document.getElementById("btnRegistro").onclick = function () {
 		 $(document).ready(function(){
     	$('.modal').modal();
  		});
-	}
+	} //valida si el campo de apellido esta lleno
 	else if($apellido.val()=='')
 	{
 		$("#btnRegistro").attr('data-target',"modal_apellido");
@@ -26,6 +29,41 @@ document.getElementById("btnRegistro").onclick = function () {
 		 $(document).ready(function(){
     	$('.modal').modal();
  		});
+	} //valida si el campo contraseña esta agregado
+	else if($contrasena.val()=='')
+	{
+		$("#btnRegistro").attr('data-target',"modal_contrasena");
+		 
+
+		 $(document).ready(function(){
+    	$('.modal').modal();
+ 		});
+	} //validar si las contraseñas son iguales
+	else if($contrasena.val()!=$contrasenaConfirma.val())
+	{
+		$("#btnRegistro").attr('data-target',"modal_contrasenaConfirma");
+		 
+
+		 $(document).ready(function(){
+    	$('.modal').modal();
+ 		});
+	}//validar si se escribio el mail
+	else if($email.val()=='')
+	{
+		$("#btnRegistro").attr('data-target',"modal_email");
+		 
+
+		 $(document).ready(function(){
+    	$('.modal').modal();
+ 		});
+	}
+	else {
+
+		alert("Datos guardados en la base de datos");
+
+
+
+		window.location.href = "index.html"
 	}
 
 	
