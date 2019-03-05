14f4156b832c55539596972dbca6cca427c3a598
armandoalamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-03-04 19:00:15 -0600

    Update Tienda.js
    implementacion del mouse hover

--- js/Tienda.js
+++ js/Tienda.js
@@ -1,2 +1,12 @@
 //Trigger del DropDown
 $('.dropdown-trigger').dropdown({coverTrigger: false, alignment: 'right', constrainWidth: false });
+
+$(function() {
+    $('.card').hover(
+        function() {
+            $(this).find('> .card-image > img.activator').click();
+        }, function() {
+            $(this).find('> .card-reveal > .card-title').click();
+        }
+    );
+})
